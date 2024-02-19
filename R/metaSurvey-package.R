b9c454925c3f89d229227ce433cacff8a6ba2d4b
Mauro Lopreten <mloprete@cognus.cl>
2024-02-18 21:04:09 -0300

    Funciones de data.table

---
+++ R/metaSurvey-package.R
@@ -0,0 +1,21 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom data.table .BY
+#' @importFrom data.table .EACHI
+#' @importFrom data.table .GRP
+#' @importFrom data.table .I
+#' @importFrom data.table .N
+#' @importFrom data.table .NGRP
+#' @importFrom data.table .SD
+#' @importFrom data.table :=
+#' @importFrom data.table data.table
+#' @importFrom lifecycle deprecated
+#' @importFrom glue glue
+#' @importFrom R6 R6Class
+#' @importFrom httr POST
+#' @importFrom httr GET
+#' @importFrom bit64 integer64
+## usethis namespace: end
+NULL
