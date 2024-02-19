b4e7e0bcd2ca2077b21dfcb8e52f54731bcc13ad
Mauro Lopreten <mloprete@cognus.cl>
2024-02-18 21:03:11 -0300

    Metadata args

---
+++ R/meta.R
@@ -0,0 +1,8 @@
+#' Metadata arguments for survey objects
+#' @return Character vector
+#' @noRd 
+#' @keywords internal
+
+metadata_args <- function() {
+    c("svy_type", "svy_edition", ".engine_name", "svy_weight")
+}
