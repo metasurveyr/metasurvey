5c8737d98f9fe35694b077649d104008f63377f1
Mauro Lopreten <mloprete@cognus.cl>
2024-03-02 23:13:49 -0300

    Close #2 Make copy optionnaly

--- R/meta.R
+++ R/meta.R
@@ -1,8 +1,8 @@
 #' Metadata arguments for survey objects
 #' @return Character vector
-#' @noRd 
+#' @noRd
 #' @keywords internal
 
 metadata_args <- function() {
-    c("svy_type", "svy_edition", ".engine_name", "svy_weight")
+  c("svy_type", "svy_edition", ".engine_name", "svy_weight")
 }
