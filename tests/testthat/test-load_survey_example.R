42ce70ef323057ea0bac144ce15a9cffd7feb7c2
Mauro Loprete <55565968+mauroloprete@users.noreply.github.com>
2024-03-10 12:52:18 -0300

    Add test load_survey-example  On branch develop  Changes to be committed: 	new file:   tests/testthat/test-load_survey_example.R

---
+++ tests/testthat/test-load_survey_example.R
@@ -0,0 +1,17 @@
+# tests/testthat/test-load_survey_example.R
+library(testthat)
+library(metaSurvey)
+
+test_that("load_survey_example works correctly", {
+  # Test that the function returns the correct files when no path is provided
+  testthat::expect_type(load_survey_example(), "character")
+
+  # Test that the function returns the correct file when a path is provided
+  testthat::expect_equal(
+    load_survey_example("2019-2021.csv"),
+    system.file("extdata", "2019-2021.csv", package = "metaSurvey", mustWork = TRUE)
+  )
+
+  # Test that the function throws an error when a non-existent path is provided
+  testthat::expect_error(load_survey_example("non_existent_file.csv"))
+})
