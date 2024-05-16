8ceac04c2bade5b474ae84e223ec7cd7545a58ca
Mauro Loprete <55565968+mauroloprete@users.noreply.github.com>
2024-05-16 20:21:58 -0300

    Add Recipe; metadata_recipe

---
+++ R/Recipes.R
@@ -0,0 +1,243 @@
+Recipe <- R6Class("Recipe",
+    public = list(
+        name = NULL,
+        edition = NULL,
+        survey_type = NULL,
+        default_engine = NULL,
+        depends_on = list(),
+        user = NULL,
+        description = NULL,
+        steps = list(),
+        initialize = function(name, edition, survey_type, default_engine, depends_on, user, description, steps) {
+            self$name <- name
+            self$edition <- edition
+            self$survey_type <- survey_type
+            self$default_engine <- default_engine
+            self$depends_on <- depends_on
+            self$user <- user
+            self$description <- description
+            self$steps <- steps
+        }
+    )
+)
+
+
+
+
+metadata_recipe <- function() {
+    return(
+        c(
+            "name",
+            "user",
+            "svy",
+            "description"
+        )
+    )
+}
+
+#' Recipe
+#' @export
+#' @param ... A list with the following metadata: name, user, svy, description
+#' @return A Recipe object
+
+recipe <- function(...) {
+    
+    dots <- substitute(list(...))
+
+    metadata_recipes_names <- metadata_recipe()
+
+    check_args = sum(metadata_recipes_names %in% names(dots))
+
+
+
+    if (!(check_args == length(metadata_recipes_names))) {
+        stop(
+            message(
+                "The recipe must have the following metadata: ",
+                paste(metadata_recipe(), collapse = ", ")
+            )
+        )
+    }
+
+    index_steps <- which(names(dots) %in% metadata_recipe())
+
+
+    if ("steps" %in% names(dots)) {
+        return(
+            Recipe$new(
+                name = dots$name,
+                user = dots$user,
+                edition = dots$svy$edition,
+                survey_type = dots$svy$type,
+                default_engine = default_engine(),
+                depends_on = list(),
+                description = dots$description,
+                steps = eval(dots$steps)
+            )
+        )
+    } else {
+        return(
+            Recipe$new(
+                name = dots$name,
+                user = dots$user,
+                edition = dots$svy$edition,
+                survey_type = dots$svy$type,
+                default_engine = default_engine(),
+                depends_on = list(),
+                description = dots$description,
+                steps = dots[-index_steps]
+            )
+        )
+    }
+}
+
+#' Encoding and decoding recipes
+#' @param recipe A Recipe object
+#' @return A Recipe object
+
+encoding_recipe <- function(recipe) {
+
+    recipe$steps <- lapply(recipe$steps, function(step) {
+        step_string <- deparse(step)
+        return(step_string)
+    })
+
+    return(recipe)
+}
+
+#' Encoding and decoding recipes
+#' @param recipe A Recipe object
+#' @return A Recipe object
+
+decode_recipe <- function(recipe) {
+    recipe$steps <- as.call(
+        lapply(
+            recipe$steps,
+            function(step_string) as.call(parse(text = step_string))[[1]]
+        )
+    )
+
+    return(
+        recipe
+    )
+}
+
+#' Save a recipe to a file
+#' @param recipe A Recipe object
+#' @param file A character string with the file path
+#' @importFrom jsonlite write_json
+#' @return NULL
+#' @export
+
+save_recipe <- function(recipe, file) {
+
+    recipe = list(
+        name = recipe$name,
+        user = recipe$user,
+        svy_type = recipe$survey_type,
+        edition = recipe$edition,
+        description = recipe$description,
+        steps = recipe$steps
+    )
+
+    recipe |> 
+        encoding_recipe() |> 
+        write_json(path = file, simplifyVector = TRUE)
+    
+    message(
+        glue::glue("The recipe has been saved in {file}")
+    )
+}
+
+#' Load a recipe from a file
+#' @param file A character string with the file path
+#' @importFrom jsonlite read_json
+#' @return A Recipe object
+#' @export
+
+read_recipe <- function(file) {
+    
+    file |> 
+        read_json(simplifyVector = TRUE) |> 
+        decode_recipe()
+
+}
+
+#' Get a recipe from the API
+#' @param topic A character string with the topic of the recipe
+#' @param svy_type A character string with the survey type of the recipe
+#' @param svy_edition A character string with the survey edition of the recipe
+#' @importFrom httr POST
+#' @importFrom jsonlite parse_json
+#' @importFrom httr content
+#' @importFrom httr add_headers
+#' @return A Recipe object
+#' 
+#' @export
+
+get_recipe <- function(
+    svy_type = NULL,
+    svy_edition = NULL,
+    topic = NULL
+) {
+
+    filterList = list(
+        svy_type = svy_type,
+        svy_edition = svy_edition,
+        topic = topic
+    )
+
+    filterList <- filterList[!sapply(filterList, is.null)]
+
+    baseUrl = "https://sa-east-1.aws.data.mongodb-api.com/app/data-vonssxi/endpoint/data/v1/action/"
+    
+    url = paste0(
+        baseUrl,
+        "findOne"
+    )
+    
+    headers <- c(
+        "Content-Type" = "application/json",
+        "Access-Control-Request-Headers" = "*",
+        "api-key" = "MKwpkpQCX1meBSN6jmsS5XpIiPvJgfOdxzjinsDC83AX5Mx18j3o16cdhtgPYXQj"
+    )
+
+    body <- list(
+        collection = "recipes",
+        database = "metasurvey",
+        dataSource = "Cluster0",
+        filter = filterList
+    )
+
+    response <- POST(
+        url, 
+        body = body,
+        encode = "json",
+        add_headers(.headers = headers)
+    )
+
+    recipe <- decode_recipe(parse_json(content(response, "text", encoding = "UTF-8"))[['document']])
+    # return(content(response, "text", encoding = "UTF-8"))
+    return(recipe)
+}
+
+#' Convert a list of steps to a recipe
+#' @param steps A list with the steps of the recipe
+#' @return A Recipe object
+#' @export
+
+steps_to_recipe <- function(steps) {
+    return(
+        recipe(
+            user = "Mauro Loprete",
+            svy = survey_empty(type = "eaii", edition = "2019-2021"),
+            description = "Receta para la encuesta de Actividad Industrial, Comercial y de Servicios",
+            steps = unname(lapply(
+                steps,
+                function(step) {
+                    unname(step$call)
+                }
+            ))
+        )
+    )
+}
