94533c0c7a958a06d480387cc75e05f472554d35
Mauro Lopreten <mloprete@cognus.cl>
2024-05-30 17:30:05 -0300

    Add design to survey objects

---
+++ R/workflow.R
@@ -0,0 +1,137 @@
+#' @title Workflow
+#' @description Workflow
+#' @keywords Surveymethods
+#' @param survey Survey object
+#' @param ... Calls
+#' @importFrom data.table rbindlist
+#' @export
+#' @return Data
+#'
+workflow <- function(survey, ...) {
+  .calls <- substitute(list(...))
+  lapply(
+    X = 1:length(survey),
+    function(i) {
+      survey <- survey[[i]]
+
+      result <- rbindlist(
+        lapply(
+          2:length(.calls),
+          function(i) {
+            call <- as.list(.calls[[i]])
+            name_function <- deparse(call[[1]])
+            call[["design"]] <- substitute(design)
+            call <- as.call(call)
+            estimation <- eval(call, envir = list(design = survey$design))
+
+
+
+            return(
+              cat_estimation(estimation, name_function)
+            )
+          }
+        )
+      )
+
+      return(
+        list(
+          survey = survey,
+          calls = .calls,
+          result = result
+        )
+      )
+    }
+  )
+}
+
+cat_estimation <- function(estimation, call) {
+  class_estimation <- class(estimation)[1]
+
+  if (class_estimation != "svyby" & class_estimation != "svyratio") {
+    class_estimation <- "default"
+  }
+
+  print(paste0(
+    "cat_estimation.",
+    class_estimation
+  ))
+
+  do.call(
+    paste0(
+      "cat_estimation.",
+      class_estimation
+    ),
+    list(
+      estimation,
+      call
+    )
+  )
+}
+
+#' cat_estimation_svyby
+#' @param estimation Estimation
+#' @param call Call
+#' @importFrom data.table data.table melt
+#' @keywords internal
+#' @noRd
+
+cat_estimation.svyby <- function(estimation, call) {
+  dt <- data.table(estimation)
+
+  se_cols <- grep("^se\\.", names(dt), value = TRUE)
+
+  est_cols <- setdiff(names(dt), se_cols)
+
+  dt_melted <- melt(dt,
+    measure.vars = est_cols,
+    variable.name = "stat",
+    value.name = "value"
+  )
+
+  for (col in se_cols) {
+    stat_name <- gsub("^se\\.", "", col)
+    dt_melted[stat == stat_name, se := dt[[col]]]
+  }
+
+  return(dt_melted)
+}
+
+
+#' cat_estimation_default
+#' @param estimation Estimation
+#' @param call Call
+#' @importFrom data.table data.table
+#' @importFrom survey SE cv
+#' @importFrom stats coef
+#' @keywords internal
+
+cat_estimation.default <- function(estimation, call) {
+  dt <- data.table(
+    stat = paste0(call, ": ", names(estimation)),
+    value = coef(estimation),
+    se = unname(SE(estimation)),
+    cv = unname(cv(estimation))
+  )
+  names(dt) <- c("stat", "value", "se", "cv")
+  return(dt)
+}
+
+#' cat_estimation_svyratio
+#' @param estimation Estimation
+#' @param call Call
+#' @importFrom data.table data.table
+#' @importFrom survey SE cv
+#' @importFrom stats coef
+#' @keywords internal
+#' @noRd
+
+cat_estimation.svyratio <- function(estimation, call) {
+  dt <- data.table(
+    stat = paste0(call, ": ", names(SE(estimation))),
+    value = coef(estimation),
+    se = unname(SE(estimation)),
+    cv = unname(cv(estimation))
+  )
+  names(dt) <- c("stat", "value", "se", "cv")
+  return(dt)
+}
